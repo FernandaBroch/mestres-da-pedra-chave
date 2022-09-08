20d5481984eb8084e533b232f096969f9da9c648
FernandaBroch <fernanda.broch@gmail.com>
2022-09-07 22:02:46 -0300

    Add calculate team score logic

--- characterProfile.js
+++ characterProfile.js
@@ -3,11 +3,27 @@
 const BASE_URL = 'http://127.0.0.1:5500/raiderio.json';
 //const BASE_URL = "https://raider.io/api/v1/characters/profile";
 
+let championshipDate = new Date("2022-09-04T00:21:42.000Z")
 let region = "us"
 let realm = "Stormrage"
 let charName = "Shampug"
 let fields = "mythic_plus_recent_runs"
-const GET_URL = `${BASE_URL}?region=${region}&realm=${realm}&name=${charName}&fields=${fields}`      
+const GET_URL = `${BASE_URL}?region=${region}&realm=${realm}&name=${charName}&fields=${fields}`
+
+let raiderIODungeonData = []
+
+let convertDateToJSDate = (date) => {
+  return new Date(date)
+}
+
+let calculateMythicScore = (runData) => {
+  let championshipScore = runData.reduce((score, obj) => { 
+    if (convertDateToJSDate(obj.completed_at) > championshipDate)
+      return score + obj.score
+    return score
+  }, 0);
+  return championshipScore
+}
 
 class CharacterIOCard extends React.Component {
   constructor(props) {
@@ -15,7 +31,8 @@ class CharacterIOCard extends React.Component {
     this.state = {
       error: null,
       isLoaded: false,
-      items: []
+      result: null,
+      raiderDungeonIoData: []
     };
   }
 
@@ -26,9 +43,9 @@ class CharacterIOCard extends React.Component {
         (result) => {
           this.setState({
             isLoaded: true,
-            items: [result]
+            result: result,
+            raiderIODungeonData: result.mythic_plus_recent_runs
           });
-          console.log(result)
         },
         (error) => {
           this.setState({
@@ -40,20 +57,18 @@ class CharacterIOCard extends React.Component {
   }
 
   render() {
-    const { error, isLoaded, items } = this.state;
+    const { error, isLoaded, result, raiderIODungeonData } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
-    } else if (items) {
+    } else if (result) {
       return (
-        <ul>
-          {items.map(item => (
-            <li key="1">
-              {item.name} {item.race} {item.achievement_points}
-            </li>
-          ))}
-        </ul>
+        <div>
+          <img src={result.thumbnail_url} alt="Character Image" />
+          <h3>{result.name}</h3>
+          <span>{calculateMythicScore(raiderIODungeonData)}</span>
+        </div>
       );
     }
   }
