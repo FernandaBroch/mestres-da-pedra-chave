af81110c203763533b00b2d363751f0d1ede1241
FernandaBroch <fernanda.broch@gmail.com>
2022-09-08 00:50:30 -0300

    Add card do time com pontos

--- characterProfile.js
+++ characterProfile.js
@@ -10,19 +10,19 @@ let charName = "Shampug"
 let fields = "mythic_plus_recent_runs"
 const GET_URL = `${BASE_URL}?region=${region}&realm=${realm}&name=${charName}&fields=${fields}`
 
-let raiderIODungeonData = []
-
 let convertDateToJSDate = (date) => {
   return new Date(date)
 }
 
-let calculateMythicScore = (runData) => {
-  let championshipScore = runData.reduce((score, obj) => { 
-    if (convertDateToJSDate(obj.completed_at) > championshipDate)
-      return score + obj.score
-    return score
-  }, 0);
-  return championshipScore
+let filterMythicPlusChampionshipRuns = (mythicPlusRuns) => {
+  return mythicPlusRuns.filter((currentValue) => {
+    return convertDateToJSDate(currentValue.completed_at) > championshipDate      
+  })  
+}
+
+let calculateMythicScore = (mythicPlusRecentRuns) => {
+  let mythicPlusChampionshipRuns = filterMythicPlusChampionshipRuns(mythicPlusRecentRuns)
+  return mythicPlusChampionshipRuns.reduce((score, obj) => { return score + obj.score }, 0);  
 }
 
 class CharacterIOCard extends React.Component {
@@ -32,7 +32,7 @@ class CharacterIOCard extends React.Component {
       error: null,
       isLoaded: false,
       result: null,
-      raiderDungeonIoData: []
+      mythicPlusRecentRuns: []
     };
   }
 
@@ -44,7 +44,7 @@ class CharacterIOCard extends React.Component {
           this.setState({
             isLoaded: true,
             result: result,
-            raiderIODungeonData: result.mythic_plus_recent_runs
+            mythicPlusRecentRuns: result.mythic_plus_recent_runs
           });
         },
         (error) => {
@@ -57,17 +57,35 @@ class CharacterIOCard extends React.Component {
   }
 
   render() {
-    const { error, isLoaded, result, raiderIODungeonData } = this.state;
+    const { error, isLoaded, result, mythicPlusRecentRuns } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else if (result) {
+      let score = calculateMythicScore(mythicPlusRecentRuns)
       return (
-        <div>
-          <img src={result.thumbnail_url} alt="Character Image" />
-          <h3>{result.name}</h3>
-          <span>{calculateMythicScore(raiderIODungeonData)}</span>
+        <div className="row">
+          
+            <div className="card">
+              <div className="card-image">
+                <img src={result.thumbnail_url} alt="Character Image" />
+                <span className="card-title">{result.name}</span>
+              </div>
+              <div className="card-content">
+                <h3 className="card-title activator center grey-text text-darken-4">{score}<i className="material-icons right">more_vert</i></h3>
+              </div>
+              <div className="card-reveal">
+                <span className="card-title grey-text text-darken-4 center">{score}<i className="material-icons right">close</i></span>                
+                <ul className="collection">
+                  {filterMythicPlusChampionshipRuns(mythicPlusRecentRuns).map((item, index) => (
+                    <li key={index} className="collection-item">
+                      {item.dungeon}: <span className="right">{item.score}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>            
+          </div>
         </div>
       );
     }
